dfbb125a017ec9d30c64f0c5134d579995d25a0f
rock2107 <104752215+rock2107@users.noreply.github.com>
2023-05-15 12:31:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Etickets.Data;
+using Etickets.Data.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+//DATABASE CONNECTION
+builder.Services.AddDbContext<AppDbContext>(Options =>
+         Options.UseSqlServer(builder.Configuration.GetConnectionString("DefultConnectionString"))); 
+builder.Services.AddScoped<IActorService,ActorService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+AppDbInitializer.Seed(app);
+
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+
+
